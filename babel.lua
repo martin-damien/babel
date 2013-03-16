ada6498efed3f6da84bd3d8aed8184ac42034c51
Damien MARTIN <damien@martin-damien.fr>
2013-03-16 13:46:55 +0100

    Merge changes from Roland Yonaba

--- babel.lua
+++ babel.lua
@@ -23,8 +23,13 @@
 
         KOLL Thomas R.
 
-            Github:  https://github.com/TomK32
+            Github:  http://github.com/TomK32
             Website: http://ananasblau.com
+
+        YONABA Roland
+
+            Github: http://github.com/Yonaba
+            Website: http://yonaba.github.com
 
 ]]
 
@@ -38,19 +43,24 @@ babel.locales_folders = {}      -- List of all the folders look in
 -- This make Babel usable for LÖVE application and
 -- standalone applications.
 
-local in_love = love or nil
+local in_love = love
 local file_exists = function() end
 local load = function() end
 
 if not in_love then
     -- We are not in LÖVE so we must use lfs instead
-    require "lfs"
+    lfs = require "lfs"
     file_exists = function( file )
         local test, err_msg = lfs.attributes( file )
         if test then return true else return false end
     end
     load = function( file )
-        return loadfile( file )
+				local chunk, msg = loadfile(file)
+				if not chunk then
+					msg = msg:gsub('^%w+%.lua:%d+:','')
+					return error(('Error reading locale file: "%s":\n\t%s'):format(file,msg),2)
+				end
+        return chunk
     end
 else
     -- We are in LÖVE
@@ -58,11 +68,26 @@ else
     load = love.filesystem.load
 end
 
+--- Merge two tables in one. t2 elements will be added to t1 and t2 elements will
+-- override existing elements in t2.
+-- @param t1 The table who will be used to be merged.
+-- @param t2 The tabls who will be merged with.
+-- @return The merge tables.
+local mergeTables = function( t1, t2 )
+
+    for k, v in pairs( t2 ) do
+        t1[k] = v
+    end
+
+    return t1
+
+end
+
 --- Init babel with the wished values.
 -- @param settings A table with all the needed settings for babel.
 babel.init = function( settings )
 
-    babel.current_locale  = settings.locale or "eng-UK"
+    babel.current_locale  = settings.locale or "en-UK"
     babel.locales_folders = settings.locales_folders or { "translations" }
     babel.debug           = settings.debug or false
 
@@ -91,29 +116,30 @@ babel.switchLocale = function( locale )
 
     locale = locale or babel.current_locale
 
-    babel.reset()
-
-    if locale == nil or locale == '' then
+    if not locale or locale == '' then
       return
     end
 
-    for _, folder in pairs( babel.locales_folders ) do
+    babel.reset()
 
-        if file_exists( folder .. "/" .. locale .. ".lua" ) then
+    for _, folder in pairs( babel.locales_folders ) do
+				local locale_file = ("%s/%s.lua"):format(folder, locale)
+
+        if file_exists( locale_file ) then
 
             if babel.debug then
-                print( "BABEL : Loading " .. folder .. "/" .. locale .. ".lua" )
+                print( ("BABEL : Loading : %s"):format(locale_file))
             end
 
-            babel.current_locale = locale
-
-            chunk = load( folder .. "/" .. locale .. ".lua" )
+            local chunk = load( locale_file )
             language = chunk()
+						babel.current_locale = locale
+            babel.formats = mergeTables( babel.formats, language.formats or {} )
+            babel.dictionary = mergeTables( babel.dictionary, language.translations or {} )
 
-            babel.formats = babel.mergeTables( babel.formats, language.formats or {} )
-            babel.dictionary = babel.mergeTables( babel.dictionary, language.translations or {} )
-
-        end
+        else
+					return error(('Cannot find locale file: "%s"'):format(locale_file))
+				end
 
     end
 
@@ -123,14 +149,13 @@ end
 --- Translate a string to the current locale (dynamic texts could be inserted)
 -- @param string The text to translate.
 -- @param parameters A list of all the dynamic elements in the string
-babel.translate = function( string, parameters )
+babel.translate = function( str, parameters )
 
     local parameters = parameters or {}
-    local translation = string
+    local translation
 
-    if babel.dictionary[string] ~= nil then
-        translation = babel.dictionary[string]
-    end
+    if not babel.dictionary[str] then return end
+    translation = babel.dictionary[str]
 
     -- Replace parameters
     for key, value in pairs( parameters ) do
@@ -138,22 +163,6 @@ babel.translate = function( string, parameters )
     end
 
     return translation
-
-end
-
-
---- Merge two tables in one. t2 elements will be added to t1 and t2 elements will
--- override existing elements in t2.
--- @param t1 The table who will be used to be merged.
--- @param t2 The tabls who will be merged with.
--- @return The merge tables.
-babel.mergeTables = function( t1, t2 )
-
-    for k, v in pairs( t2 ) do
-        t1[k] = v
-    end
-
-    return t1
 
 end
 
